a9e1ff2eccd0a9be8ddf168b08bbdc052ff947e4
jannik <jannik.m.buhr@gmail.com>
2020-02-11 10:33:03 +0100

    add environment variables

--- R/switchTheme.R
+++ R/switchTheme.R
@@ -12,9 +12,12 @@ NULL
 
 #' switchTheme
 #'
-#' This funcions checks if the current theme is `...`. If this
-#' is the case, it changes the light variant (`...`).
+#' This funcions checks if the current theme is dark. If this
+#' is the case, it changes to the light variant.
 #' Likewise, the theme is changed to the dark one if it wasn't previously.
+#'
+#' Use configureDarkTheme and configureLightTheme to set
+#' the environment variables for your preferred themes
 #'
 #' @return
 #' NULL
@@ -24,9 +27,42 @@ NULL
 #' # Use the addins browser or activate via the addins menue to switch between themes.
 #'
 switchTheme <- function(){
-  if (rstudioapi::getThemeInfo()$dark) {
-    rstudioapi::applyTheme("Textmate (default)")
-  } else {
-    rstudioapi::applyTheme("rscodeio")
+  e <- function(e) {
+  stop("Could not apply theme, please refer to themeSwitch::configureDarkTheme()
+            and themeSwitch::configureLightTheme() to set the environment variables")
   }
+  if (rstudioapi::getThemeInfo()$dark) {
+    tryCatch({
+    rstudioapi::applyTheme(Sys.getenv("RSTUDIO_LIGHT_THEME"))
+    }, error = e)
+  } else {
+    tryCatch({
+      rstudioapi::applyTheme(Sys.getenv("RSTUDIO_DARK_THEME"))
+    }, error = e)
+  }
+}
+
+
+#' Configure current theme as dark theme
+#'
+#' Uses an environment variable RSTUDIO_DARK_THEME
+#'
+#' @return
+#' @export
+#'
+#' @examples
+configureDarkTheme <- function() {
+  Sys.setenv(RSTUDIO_DARK_THEME = rstudioapi::getThemeInfo()$editor)
+}
+
+#' Configure current theme as light theme
+#'
+#' Uses an environment variable RSTUDIO_LIGHT_THEME
+#'
+#' @return
+#' @export
+#'
+#' @examples
+configureLightTheme <- function() {
+  Sys.setenv(RSTUDIO_LIGHT_THEME = rstudioapi::getThemeInfo()$editor)
 }
--- README.md
+++ README.md
@@ -1,8 +1,29 @@
 # themeSwitch
-Switch your RStudio theme at the press of a button
+
+Switch your RStudio theme at the press of a button.
 
 install via
 
 ```r
 devtools::install_github("jannikbuhr/themeSwitch")
 ```
+
+Then switch to your prefered dark theme and run
+
+```r
+configureDarkTheme()
+```
+
+Switch to your prefered light theme and run
+
+```r
+configureLightTheme()
+```
+
+These functions will set the environment variables
+that are used by `switchTheme()` as your themes.
+For maximum efficiency, I recommend to set a shortcut
+for the addin browser (I use Ctrl+Shift+P, like the command
+palette in VS Code). Then you hit that shortcut, start typing
+_switch..._ and as soon as `switchTheme` is the only
+visible entry in the search list, hit Enter.
